0e675ef27907869448a16c09c4fece7135cdb6cc
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2024-01-24 18:15:29 +0100

    my_sort and predicateSort are now two different functions. example-data now included as a variable.

--- lists.hs
+++ lists.hs
@@ -1,3 +1,5 @@
+example = ["alfred", "Alfred", "Isac", "Erik", "Zlatan", "Ronaldo", "Zendaya", "Timothy Chamalet"]
+
 
 my_filter :: (a->Bool) -> [a] -> [a]
 my_filter _ [] = []
@@ -7,13 +9,8 @@ my_filter predicate (x:xs)
 
 -- Sorts the provided List ascendingly, using a bubble-like sort. 
 my_sort :: (Ord a) => [a] -> [a]
-my_sort arr
-    | (length arr) > 1 = (my_sort (init bubbled)) ++ [last bubbled]
-    | otherwise = arr
-    where
-        -- If predicate returns true, then elements x and y swap places
-        pred = (\x y -> y<x)
-        bubbled = (bubble pred arr)
+my_sort arr = predicateSort (\x y -> x>y) arr
+
 bubble :: (Ord a)=> (a->a->Bool) -> [a] -> [a]
 bubble _ [] = []
 bubble predicate (a:(b:xs)) 
@@ -22,3 +19,12 @@ bubble predicate (a:(b:xs))
 bubble predicate xs
     | predicate (head xs) (last xs) = reverse xs
     | otherwise = xs
+
+predicateSort :: (Ord a )=> (a->a->Bool) -> [a]-> [a]
+predicateSort pred arr
+    | (length arr) > 1 = (predicateSort pred (init bubbled)) ++ [last bubbled]
+    | otherwise = arr
+    where
+        bubbled = (bubble pred arr)
+
+
