cdc416078b1683604c2684accd1f0be2249cdef1
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2024-01-24 18:01:09 +0100

    Changes have been made to my_sort, now predicate is not needed in the call. Bubble actually uses the predicate now, so it can be fixed in the future

--- lists.hs
+++ lists.hs
@@ -5,19 +5,20 @@ my_filter predicate (x:xs)
     | predicate x = [x] ++ my_filter predicate xs
     | otherwise = filter predicate xs
 
-
--- If predicate returns true, then A and B shall swap places.
-my_sort :: (Ord a, Num a) => (a->b->Bool) -> [a] -> [a]
-my_sort pred arr 
-    | (length arr) > 1 = my_sort pred (init bubbled) ++ [last bubbled]
+-- Sorts the provided List ascendingly, using a bubble-like sort. 
+my_sort :: (Ord a) => [a] -> [a]
+my_sort arr
+    | (length arr) > 1 = (my_sort (init bubbled)) ++ [last bubbled]
     | otherwise = arr
-    where 
+    where
+        -- If predicate returns true, then elements x and y swap places
+        pred = (\x y -> y<x)
         bubbled = (bubble pred arr)
-bubble :: (Ord a, Num a)=> (a->b->Bool) -> [a] -> [a]
+bubble :: (Ord a)=> (a->a->Bool) -> [a] -> [a]
 bubble _ [] = []
 bubble predicate (a:(b:xs)) 
-    | a > b = [b] ++ bubble predicate ([a] ++ xs)
+    | predicate a b = [b] ++ bubble predicate ([a] ++ xs)
     | otherwise = [a] ++ bubble predicate ([b] ++ xs)
 bubble predicate xs
-    | (head xs) > (last xs) = reverse xs
+    | predicate (head xs) (last xs) = reverse xs
     | otherwise = xs
